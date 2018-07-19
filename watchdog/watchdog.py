a9467dc3b6d7476798df427d2a10c7d4f9bafff7
Wojciech Kalinski <wojciechmkalinski@gmail.com>
2018-07-19 13:06:29 +0200

    Fixing methods in script to make it more readable

--- watchdog/watchdog.py
+++ watchdog/watchdog.py
@@ -2,71 +2,73 @@ import os
 
 import boto3
 import subprocess
-import json
-from botocore.exceptions import ClientError
-
 from time import sleep
 
-import daemon
 
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
+table_name="rnd-coding-challenge-wojciechmkalinski-gmailcom-WatchdogTable-1T6M0MOPDKMI2"
+topic_arn="rnd-coding-challenge-wojciechmkalinski-gmailcom-WatchdogSnsTopic-YYX9A9SCNG2Z"
 
 sns = boto3.client("sns", region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY,
                    aws_secret_access_key=AWS_SECRET_KEY)
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-watchdog_table = dynamodb.Table("rnd-coding-challenge-wojciechmkalinski-gmailcom-WatchdogTable-1T6M0MOPDKMI2")
+dynamodb = boto3.resource('dynamodb')
+watchdog_table = dynamodb.Table(table_name)
 array_of_services = ['spotify', 'java']
 
 
 def insert_row():
-    dynamodb.put_item(
-        TableName="rnd-coding-challenge-wojciechmkalinski-gmailcom-WatchdogTable-1T6M0MOPDKMI2",
+    watchdog_table.put_item(
         Item={
-            'Id': "1",
-            'ListOfServices': ["mysqld", "java"],
-            'NumOfSecCheck': 60,
-            'NumOfSecWait': 10,
-            'NumOfAttempts': 4
+            'id': "1",
+            'ListOfServices': ["spotify", "java"],
+            'NumOfSecCheck': '60',
+            'NumOfSecWait': '10',
+            'NumOfAttempts': '4'
         }
     )
 
 
-def check_services(row):
-    num_of_sec_wait = row.NumOfSecWait
-    num_of_attempts = row.NumOfAttempts
+def publish_sns(message):
+    sns.publish(
+        TargetArn=topic_arn,
+        Message=message
+    )
+
+
+def check_services(id):
+    response = watchdog_table.get_item(
+        TableName=table_name,
+        Key={
+            'id': id
+        }
+    )
+    item = response['Item']
+    num_of_sec_check = item['NumOfSecCheck']
+    num_of_sec_wait = item['NumOfSecWait']
+    num_of_attempts = item['NumOfAttempts']
+    services = item['ListOfServices']
     p = subprocess.Popen(["ps", "-a"], stdout=subprocess.PIPE)
     attempts = 0
     out, err = p.communicate()
-    for service in array_of_services:
+    for service in services:
         if service in str(out):
-            sns.publish(
-                TargetArn="rnd-coding-challenge-wojciechmkalinski-gmailcom-WatchdogSnsTopic-YYX9A9SCNG2Z",
-                Message=json.dumps()
-            )
-            print(service + " running")
+            publish_sns(service + " has been started after " + attempts + " attempts." )
         else:
             while attempts < num_of_attempts:
                 attempts += 1
-                print(attempts + " check of " + service + " not running.")
+                publish_sns(service + " is not running after " + attempts + " attempts.")
                 sleep(num_of_sec_wait)
+                continue
+            if attempts >= num_of_attempts:
+                publish_sns(service + " is down.")
+        sleep(num_of_sec_check)
 
 
-def get_row():
-    try:
-        response = watchdog_table.get_item(
-            Key={
-                'Id': '1'
-            }
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-        item = response('Item')
-    return item
-
-
-def main():
+def main(row_id):
     insert_row()
-    db_row = get_row()
-    check_services(db_row)
+    check_services(row_id)
+
+
+main(1)
